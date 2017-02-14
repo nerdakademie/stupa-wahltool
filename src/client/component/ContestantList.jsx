d78a273cf7eb5967293c64100ef8f44dc4bf2796
Michel Wohlert <michel.wohlert@gmail.com>
2017-02-14 13:54:05 +0100

    Added Shadown to Card in ContestantList

--- src/client/component/ContestantList.jsx
+++ src/client/component/ContestantList.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class ContestantList extends Component {
   constructor() {
     super();
-    this.state = {contestants: []};
+    this.state = {contestants: [], shadow: 1};
   }
 
   loadContestants() {
@@ -20,14 +21,28 @@ class ContestantList extends Component {
     this.loadContestants();
   }
 
+
+  onMouseOver = () => this.setState({shadow: 3})
+  onMouseOut = () => this.setState({shadow: 1})
+
   static createCard(contestant) {
+    const style = {
+      margin: 12,
+    };
     return (
-      <Card key={contestant._id}>
+      <Card key={contestant._id} containerStyle={{width: 300}} zDepth={this.state.shadow}
+        onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
         <CardHeader
           title={contestant.name}
           subtitle={`${contestant.year}`}
         />
       <CardText>{contestant.description}</CardText>
+      <CardActions>
+        <RaisedButton
+          label="Abstimmen"
+          style={style}
+          backgroundColor="#a4c639" />
+      </CardActions>
       </Card>
     );
   }
