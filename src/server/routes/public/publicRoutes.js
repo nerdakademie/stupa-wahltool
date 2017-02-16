beb77466f4c9f3e8dcf9ada5b7a17485677931b1
Michel Wohlert <michel.wohlert@zmmail.de>
2017-02-16 23:03:30 +0100

    change Routes and fix Appbar on top of screen

--- src/server/routes/public/publicRoutes.js
+++ src/server/routes/public/publicRoutes.js
@@ -6,6 +6,6 @@ const PublicController = require('../../controller/public/publicController');
 module.exports = class PublicRoutes extends Express.Router {
   constructor() {
     super();
-    this.get('/', PublicController.index);
+    this.get('/*', PublicController.index);
   }
 };
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -9,8 +9,8 @@ const ErrorRoutes = require('./errorRoutes');
 module.exports = class Routes extends Express.Router {
   constructor() {
     super();
-    this.use(config.get('webserver:routes:root'), new PublicRoutes());
     this.use(`${config.get('webserver:routes:root')}/api`, new ApiRoutes());
+    this.use(config.get('webserver:routes:root'), new PublicRoutes());
 
   /*  this.get(`${config.get('turing-example:server:routes:root')}/api/status/:status/:message`, (reqest, response) => {
       turingStatus.addStatusDetail('toll', reqest.params.status, reqest.params.message);
