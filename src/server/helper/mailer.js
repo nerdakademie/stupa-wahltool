2f8dbacc6c49fc9bbf9a1f99bd7f90de4aa489d5
Malte Schlünz <malte.schluenz@nordakademie.de>
2017-02-17 00:08:12 +0100

    integrate Templates for Mails

--- src/server/helper/mailer.js
+++ src/server/helper/mailer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const NodeMailer = require('nodemailer-wrapper');
+const pug = require('pug');
 const dbConf = require('../config');
 
 module.exports = class Mailer {
@@ -23,7 +24,6 @@ module.exports = class Mailer {
       }
     };
 
-// use your mongodb address
     const
       host = dbConf.get('mongo:host');
     const
@@ -40,7 +40,6 @@ module.exports = class Mailer {
     const
       mongoUri = `mongodb://${userAndPassword}${host}:${port}/${db}`;
 
-// create new wrapper instance
     const
       mailer = new NodeMailer(mongoUri, transportConfig);
 
@@ -53,6 +52,78 @@ module.exports = class Mailer {
         html: html
       };
 
+    mailer
+      .prepareMail(mail);
+
+    mailer
+      .saveMails(
+        (err) => {
+          console
+            .info(
+              'mails have been saved !'
+            )
+          ;
+
+          mailer
+            .send(
+              (err) => {
+                if (err) {
+                  console.log(err);
+                }
+              }
+            )
+          ;
+        })
+    ;
+  }
+  static sendMailWithTemplat(config, data) {
+    const transportConfig = {
+      transportType: 'smtp',
+      config: {
+        host: 'nordakademie.de',
+        secureConnection: true,
+        port: 587,
+        auth: {
+          user: 'nerdakademie@nordakademie.de',
+          pass: 'your-password'
+        },
+        maxConnections: 20,
+        maxMessages: 10,
+        tls: {
+          rejectUnauthorized: false
+        }
+      }
+    };
+
+    const
+      host = dbConf.get('mongo:host');
+    const
+      port = dbConf.get('mongo:port');
+    const
+      db = dbConf.get('mongo:db');
+    const
+      user = dbConf.get('mongo:user');
+    const
+      password = dbConf.get('mongo:password');
+
+    const
+      userAndPassword = user && password ? `${user}:${password}@` : '';
+    const
+      mongoUri = `mongodb://${userAndPassword}${host}:${port}/${db}`;
+
+    const
+      mailer = new NodeMailer(mongoUri, transportConfig);
+
+
+    const
+      mail = {
+        from: 'nerdakademie@nordakademie.de',
+        to: config.to,
+        subject: config.subject,
+        text: pug.renderFile('../../ressources/server/view/text_' + config.template, data),
+        html: pug.renderFile('../../ressources/server/view/html_' + config.template, data)
+      };
+/* TODO Kann man mit Pug jeweils ein html und ein text template erstellen oder geht nur html? */
     mailer
       .prepareMail(mail);
 
