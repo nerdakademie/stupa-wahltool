88a7e023121ca7c19ed8dd8e406734b8945ec9ab
Michel Wohlert <michel.wohlert@gmail.com>
2017-02-21 14:30:59 +0000

    Do not save activated again if already activated

--- src/server/controller/api/contestant/contestantApiController.js
+++ src/server/controller/api/contestant/contestantApiController.js
@@ -38,42 +38,41 @@ module.exports = class ContestantApiController {
         if (count >= 1) {
           return response.status(400).json({success: false,
             error: {text: 'Du hast dich bereits aufgestellt.'}});
-        } else {
-          StudentApiController.validate(request.body, (validated) => {
-            if (validated === true) {
-              contestantJSON.activated = false;
-              contestantJSON.image = request.file.filename;
-              // sanitize user inputs
-              contestantJSON.firstName = xss(contestantJSON.firstName);
-              contestantJSON.lastName = xss(contestantJSON.lastName);
-              contestantJSON.course = xss(contestantJSON.course);
-              contestantJSON.year = xss(contestantJSON.year);
-              contestantJSON.centuria = '';
-              contestantJSON.description = xss(contestantJSON.description);
-              contestantJSON.token = '';
-              ContestantHelper.sendActivationMail(contestantJSON, (result) => {
-                if (result === false) {
-                  return response.status(400).json({
-                    success: false,
-                    error: {text: 'Fehler beim Versand der Bestätigungsmail'}
-                  });
-                }
-                return response.status(200).json({success: true});
-              });
-            } else if (validated === false && request.file.path !== undefined) {
-              fs.unlink(request.file.path, (error) => {
-                if (error) {
-                  console.log(error);
-                }
-              });
-              return response.status(400).json({
-                success: false,
-                error: {text: 'Deine Angaben konnten nicht validiert werden. \nVersuche es erneut'}
-              });
-            }
-            // return next('error');
-          });
         }
+        StudentApiController.validate(request.body, (validated) => {
+          if (validated === true) {
+            contestantJSON.activated = false;
+            contestantJSON.image = request.file.filename;
+              // sanitize user inputs
+            contestantJSON.firstName = xss(contestantJSON.firstName);
+            contestantJSON.lastName = xss(contestantJSON.lastName);
+            contestantJSON.course = xss(contestantJSON.course);
+            contestantJSON.year = xss(contestantJSON.year);
+            contestantJSON.centuria = '';
+            contestantJSON.description = xss(contestantJSON.description);
+            contestantJSON.token = '';
+            ContestantHelper.sendActivationMail(contestantJSON, (result) => {
+              if (result === false) {
+                return response.status(400).json({
+                  success: false,
+                  error: {text: 'Fehler beim Versand der Bestätigungsmail'}
+                });
+              }
+              return response.status(200).json({success: true});
+            });
+          } else if (validated === false && request.file.path !== undefined) {
+            fs.unlink(request.file.path, (error) => {
+              if (error) {
+                console.log(error);
+              }
+            });
+            return response.status(400).json({
+              success: false,
+              error: {text: 'Deine Angaben konnten nicht validiert werden. \nVersuche es erneut'}
+            });
+          }
+            // return next('error');
+        });
       });
   }
 
@@ -84,11 +83,15 @@ module.exports = class ContestantApiController {
     }
     Contestant.findOne({token: request.query.token}).exec((error, contestant) => {
       if (error || contestant === null) {
-        return response.status(400).json({success: false,
-          error: {text: 'Error while validating token'}});
+        return response.status(400).json({
+          success: false,
+          error: {text: 'Error while validating token'}
+        });
       }
-      contestant.activated = true;
-      contestant.save();
+      if (contestant.activated === false) {
+        contestant.activated = true;
+        contestant.save();
+      }
       response.writeHead(301, {Location: `${config.get('webserver:defaultProtocol')}://${config.get('webserver:url')}/list`});
       return response.end();
     });
