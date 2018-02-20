a8859ba17e175a9e66ba3710601f14583fdfa6f6
Finja Hauschild <finja.hauschild@storecast.de>
2018-02-20 23:28:21 +0100

    home

--- src/client/component/pages/Home.jsx
+++ src/client/component/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <h3>{'Willkommen bei der StuPa-Wahl 2018'}</h3>
       <p>{'Hier kannst du für die aufgestellten Kandidaten deine Stimme abgeben!'} <br />
-        {'Du hast insgesamt 4 Stimmen. Es ist möglich, dass du z.B. erst eine Person wählst und später noch welche ergänzt. Bereits abgegebene Stimmen können nicht mehr zurückgezogen werden.'}</p>
+        {'Du hast insgesamt 4 Stimmen, die in einem Vorgang genutzt werden können. Bereits abgegebene Stimmen können nicht mehr zurückgezogen werden, nicht abgegebene können nicht später vergeben werden.'}</p>
         <p>{'Der Aufstellungs-Zeitraum ist: '}<b>{'Mittwoch, 21.02. 00:00 Uhr – 25.02. 23:59 Uhr'}</b>{'. Danach beginnt die Wahl.'}</p>
         <p>{'Der Wahl-Zeitraum ist: '}<b>{'Montag, 26.02. 00:00 Uhr – 04.03. 23:59 Uhr'}</b>{'. Danach werden die künftigen Mitglieder des StuPas bekanntgegeben.'}</p>
       <p>{'Du wirst eine Mail bekommen, in der dein Token ist, mit dem du verifizierst, dass die abgegebenen Stimmen wirklich von dir kommen. Aus diesem Grund ist es wichtig, dass dein Postfach leer genug ist und dass du deine Wahlmail nicht weiterleitest!'}</p>
