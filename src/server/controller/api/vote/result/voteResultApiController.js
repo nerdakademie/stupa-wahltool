24bd0ab20de8627fd95536e5a1ed6707779b3633
Michel Wohlert <michel.wohlert@zmmail.de>
2018-03-05 04:27:20 +0100

    Make things for MasterOffice

--- src/server/controller/api/vote/result/voteResultApiController.js
+++ src/server/controller/api/vote/result/voteResultApiController.js
@@ -160,4 +160,37 @@ module.exports = class VoteResultApiController {
           error: {text: 'Datenbankfehler'}});
       });
   }
+
+  static participationPerCourseAndYear(request, response) {
+    Token.aggregate([
+      {$project: {_id: 1,
+        studentEmail: 1,
+        votes: {$cond: ['$voted', 1, 0]}}},
+      {$lookup: {from: 'students',
+        localField: 'studentEmail',
+        foreignField: 'email',
+        as: 'student'}},
+      {$match: {student: {$ne: []}}},
+      {$unwind: '$student'},
+      {$project: {votes: 1,
+        course: '$student.course',
+        year: '$student.year'}},
+      {$group: {_id: {
+        year: '$year',
+        course: '$course'},
+      votes: {$sum: '$votes'}}},
+      {$project: {_id: 0,
+        votes: 1,
+        course: '$_id.course',
+        year: '$_id.year'}},
+      {$sort: {votes: -1}}
+    ]).exec()
+      .then((participation) => {
+        return response.json(participation);
+      })
+      .catch(() => {
+        return response.status(500).json({success: false,
+          error: {text: 'Datenbankfehler'}});
+      });
+  }
 };
